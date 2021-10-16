bf2fbc4339e72d46a86bc4f71648de91a23052b9
hamidb80 <hr.bolouri@gmail.com>
2021-10-16 22:50:23 +0330

    pass tests

--- tests/test.nim
+++ tests/test.nim
@@ -1,36 +1,36 @@
 import easydb
 
 Blueprint []:
-    # Table test1:
-    #     id: int {.primary.} 
-    #     table_id: int[ref another.id]
-    #     name: char[255]
+    Table test1:
+        id: int {.primary.}
+        other_id: int[ref another.id]
+        name: char[255]
 
-    # Table members:
-    #     id: int {.primary.}
-    #     name: string[255]
+    Table members:
+        id: int {.primary.}
+        name: string[255]
 
-    # Table part:
-    #     id: int {.primary.}
-    #     name: string
+    Table part:
+        id: int {.primary.}
+        name: string
 
-    # Table quiz:
-    #     id: int {.primary.}
-    #     member_id: int[ref members.id]
-    #     name: char[255]
-    #     part_id: int[ref part.id]
+    Table quiz:
+        id: int {.primary.}
+        member_id: int[ref members.id]
+        name: char[255]
+        part_id: int[ref part.id]
 
-    # Table question:
-    #     id: int {.primary.}
-    #     quiz_id: int[ref quiz.id]
-    #     answer: int
+    Table question:
+        id: int {.primary.}
+        quiz_id: int[ref quiz.id]
+        answer: int
 
     Table record:
-        id: int {.primary.}
-        answerts: char[400]
-        question_order: char[400] {.primary.}
-        # member_id: int[ref members.id] # TODO {on_update: restrict, onDelete: restric}
-        date: char[400] {.primary, default: "val".}
+       id: int {.primary.}
+       answerts: Option[char[400]] {.default: 100.}
+       question_order: char[400] {.primary.}
+       member_id: int[ref members.id]  #TODO {on_update: restrict, onDelete: restric}
+       date: char[400] {.primary, default: "val".}
 
-    # Table test:
-    #     field: Option[char[255]]
+    Table test:
+        field: Option[char[255]]
